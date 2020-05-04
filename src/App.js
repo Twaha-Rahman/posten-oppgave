9b5ff919fb83c12c351b4eb030604821a124f4a3
Twaha Rahman <39026437+Twaha-Rahman@users.noreply.github.com>
2020-05-04 17:28:52 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import "./assets/css/core.css";
+import './assets/css/core.css';
 
 import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
 
 // Components
-import NavBar from './components/navbar/NavBar.component';
+import NavBar from './components/Navbar/NavBar.component';
 import HomePage from './Pages/homepage.component';
 import Result from './Pages/result.component';
 
 function App() {
-
   return (
     <Router>
       <header className="header">
         <NavBar />
       </header>
       <Switch>
-        <Route exact path='/' component={HomePage} />
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/result" component={Result} />
       </Switch>
-
     </Router>
   );
 }
